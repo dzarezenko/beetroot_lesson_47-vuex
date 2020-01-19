8f90851c52c4431648bc7b3bf64a632d96755f08
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2020-01-19 23:23:34 +0200

    Get posts without Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
   <div id="app">
-    
+    <div class="post" v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  async mounted() {
+    const resp = await fetch("http://jsonplaceholder.typicode.com/posts?_limit=5");
+    const posts = await resp.json();
+    console.log(posts);
+
+    this.posts = posts;
+  }
 }
 </script>
+
+<style scoped>
+.post {
+  display: block;
+  width: 300px;
+  margin: 10px auto;
+  border: 1px solid #000;
+  border-radius: 5px;
+  padding: 10px;
+}
+</style>
